7a35d8fac1b64b100f37078d4792c096b6c5e459
petervanderhook <petervanderhook@gmail.com>
2022-01-30 14:47:21 -0800

    Pushing Calculator to pages

---
+++ script.js
@@ -0,0 +1,27 @@
+window.answered = false;
+
+function addToStream(char) {
+    let stream = document.querySelector('.calculatorDisplay');
+    if (window.answered == true) {
+        stream.innerHTML = '';
+        window.answered = false;
+    }
+    stream.append(char + ' ');
+
+};
+
+function equals() {
+    let display = document.querySelector('.calculatorDisplay');
+    var stream = display.innerHTML;
+    console.log(stream);
+    answer = eval(stream);
+    console.log(`${stream} = ${answer}`)
+    display.innerHTML = `${stream} = ${answer}`;
+    window.answered = true;
+}
+
+function clearDisplay() {
+    let display = document.querySelector('.calculatorDisplay');
+    display.innerHTML = `Cleared.`;
+    window.answered = true;
+}
